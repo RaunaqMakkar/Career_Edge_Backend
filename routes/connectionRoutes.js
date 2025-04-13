a2bf7a8b78686b3aedcc31551d654f7d841446d5
Raunaq Makkar <raunaq.makkar17@gmail.com>
2025-04-13 23:16:31 +0530

    first commit

--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
-const ConnectionRequest = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/ConnectionRequest");
 const {
   createConnectionRequest,
   acceptConnectionRequest,
