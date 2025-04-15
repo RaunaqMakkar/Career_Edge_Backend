f793ff698592f4b2d60b3761607bc95d57818021
Raunaq Makkar <raunaq.makkar17@gmail.com>
2025-04-15 12:23:26 +0530

    first commit

--- index.js
+++ index.js
@@ -60,16 +60,11 @@ app.use(async (req, res, next) => {
 
 // Configure CORS to allow requests from your frontend domain
 app.use(cors({
-  origin: [
-    'https://career-edge-frontend.vercel.app',
-    'https://career-edge-frontend-m6asoq2ff-raunaqmakkars-projects.vercel.app',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
-  optionsSuccessStatus: 200
-}));
+}))
 
 // Other middleware like express.json(), etc.
 app.use(express.json());
